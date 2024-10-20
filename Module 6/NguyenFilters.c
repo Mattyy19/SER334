279a2418406a4763b21c8dd8d538239408d79346
matthew <mnguyen19045@gmail.com>
2024-10-20 11:04:20 -0700

    Added Module 6 programming

---
+++ Module 6/NguyenFilters.c
@@ -0,0 +1,137 @@
+/**
+* Takes user input to alter BMP files based on their commands
+*
+* Completion time: 30 min
+*
+* @author Matthew Nguyen
+* @version 1.0
+*/
+
+////////////////////////////////////////////////////////////////////////////////
+//INCLUDES
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <math.h>
+#include "BMPHandler.h"
+#include "Image.h"
+//TODO: finish me
+
+////////////////////////////////////////////////////////////////////////////////
+//MACRO DEFINITIONS
+
+//problem assumptions
+#define BMP_HEADER_SIZE 14
+#define BMP_DIB_HEADER_SIZE 40
+#define MAXIMUM_IMAGE_SIZE 4096
+
+//TODO: finish me
+
+
+////////////////////////////////////////////////////////////////////////////////
+//DATA STRUCTURES
+
+//TODO: finish me
+
+
+////////////////////////////////////////////////////////////////////////////////
+//MAIN PROGRAM CODE
+
+//TODO: finish me
+
+
+void main(int argc, char* argv[]) {
+	//TODO: finish me
+	//For BMP file
+	struct BMP_Header* bmpHeader = (struct BMP_Header*)malloc(sizeof(struct BMP_Header));
+	struct DIB_Header* dibHeader = (struct DIB_Header*)malloc(sizeof(struct DIB_Header));
+	struct Image* image = (struct Image*)malloc(sizeof(struct Image));
+	FILE* file;
+	struct Pixel** pArr;
+
+	//For inputs
+	char fileName[1024];
+	char outputName[1024];
+	char filterType[1024];
+	int hasOutputName = 0;
+	int hasFilter = 0;
+
+	//Opens BMP file
+	if(strcmp(argv[0], "./NguyenFilters -i") == 0)
+	{
+		//Finds input BMP file
+		file = fopen(argv[2], "rb");
+
+		//If input is invalid
+		if(file == NULL)
+		{
+			printf("%s was not found.\n", argv[1]);
+		}
+
+		strcpy(fileName, argv[1]);
+
+		//Reads BMP, DIP header, and pixel array
+		readBMPHeader(file, bmpHeader);
+		readDIBHeader(file, dibHeader);
+
+		pArr = (struct Pixel**)malloc(sizeof(struct Pixel*) * dibHeader->imageHeight);
+		int p;
+		for (p = 0; p < dibHeader->imageHeight; p++) {
+			pArr[p] = (struct Pixel*)malloc(sizeof(struct Pixel) * dibHeader->imageWidth);
+		}
+
+		readPixelsBMP(file, pArr, dibHeader->imageWidth, dibHeader->imageHeight);
+		image = image_create(pArr, dibHeader->imageWidth, dibHeader->imageHeight);
+
+		fclose(file);
+	}
+
+	//Loops through argv to find command arguments
+	int i;
+	for(i = 3; i < argc; i++)
+	{
+		//Makes output file name
+		if(strcmp(argv[i], "-o") == 0)
+		{
+			strcpy(outputName, argv[i + 1]);
+			hasOutputName = 1;
+		}
+
+		//Has filter
+		if(strcmp(argv[i], "-f") == 0)
+		{
+			strcpy(filterType, argv[i + 1]);
+			hasFilter = 1;
+		}
+	}
+
+	//Blur filter selected
+	if(strcmp(filterType, "b") == 0)
+	{
+		//TODO
+	}
+
+	//Cheese filter selected
+	if(strcmp(filterType, "c") == 0)
+	{
+		//TODO
+	}
+
+	//If output was used as an option
+	if(hasOutputName == 1)
+	{
+		FILE* fileOut = fopen(outputName, "wb");
+
+		//Writes to BMP file
+		writeBMPHeader(fileOut, bmpHeader);
+		writeDIBHeader(fileOut, dibHeader);
+		writePixelsBMP(fileOut, image_get_pixels(image), dibHeader->imageWidth, dibHeader->imageHeight);
+		fclose(fileOut);
+	}
+
+	free(bmpHeader);
+	free(dibHeader);
+	free(pArr);
+	image_destroy(&image);
+}
